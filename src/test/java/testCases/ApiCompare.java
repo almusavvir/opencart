cfe2e574d6e99d0eec28c834f409964437a14ee4
almusavvir/devbase <almusavvir.siddiqui@gmail.com>
2025-02-09 21:18:37 +0530

    add api compare

--- src/test/java/testCases/ApiCompare.java
+++ src/test/java/testCases/ApiCompare.java
@@ -1,7 +1,30 @@
 package testCases;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.FileReader;
+import java.io.IOException;
+
 public class ApiCompare {
-    public void apiCompare () {
+
+    @Test(priority = 0)
+    public static void apiCompare() throws IOException, ParseException {
+
+        JSONObject obj1 = (JSONObject) new JSONParser().parse(new FileReader("C:\\Users\\devbase\\Projects\\opencart\\resources1\\CatalogAPI.json"));
+        JSONObject obj2 = (JSONObject) new JSONParser().parse(new FileReader("C:\\Users\\devbase\\Projects\\opencart\\resources1\\HybrisAPI.json"));
+
+        ObjectMapper mapper = new ObjectMapper();
+        obj1.
+
+        Assert.assertEquals(mapper.readTree(obj1.toJSONString()), mapper.readTree(obj2.toJSONString()));
 
     }
+
+    //public static void main(String[] args) throws IOException, ParseException {
+        //
 }
